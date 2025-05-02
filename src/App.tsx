b99cfb17c23713fb3e937f5d1f18e062772d36a0
JKnight3517 <justin@thevessale.com>
2025-05-02 00:51:17 -0700

    Get rid of navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { NavigationBar } from './features/shared/components/NavigationBar';
 import { RestaurantPage } from './features/restaurant/components/RestaurantPage';
 import { CartProvider } from './features/cart/context/CartContext';
 import { CheckoutPage } from './features/payment/components/CheckoutPage';
@@ -33,7 +32,6 @@ function AppContent() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <NavigationBar />
       <Routes>
         {/* Restaurant routes - now as top-level routes */}
         <Route path="/:restaurantSlug" element={<RestaurantPage />} />
